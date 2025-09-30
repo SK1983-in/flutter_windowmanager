b5b143ae022afa85d9d6df533af5d2c58487cf61
SK316 <sentil1983@gmail.com>
2025-09-30 13:34:28 +0530

    registrar

--- android/src/main/java/io/adaptant/labs/flutter_windowmanager/FlutterWindowManagerPlugin.java
+++ android/src/main/java/io/adaptant/labs/flutter_windowmanager/FlutterWindowManagerPlugin.java
@@ -1,23 +1,46 @@
 package io.adaptant.labs.flutter_windowmanager;
 
-import android.app.Activity;
-import android.view.WindowManager;
-import androidx.annotation.NonNull;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodChannel;
+import android.os.Build;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
-public class FlutterWindowManagerPlugin implements FlutterPlugin {
+public class FlutterWindowManagerPlugin implements FlutterPlugin, MethodCallHandler {
 
     private MethodChannel channel;
+    private Activity activity;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
         channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_windowmanager");
-        // handle method calls
+        channel.setMethodCallHandler(this);
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
     }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("addFlagsSecure")) {
+            if (activity != null) {
+                activity.runOnUiThread(() -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+                    }
+                });
+                result.success(true);
+            } else {
+                result.error("NO_ACTIVITY", "Activity not attached", null);
+            }
+        } else {
+            result.notImplemented();
+        }
+    }
 }
+
